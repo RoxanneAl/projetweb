6991d8aebdb58357b5dca89d0569a61b9aeb6cc5
ElieDevriendtIMT <145541797+ElieDevriendtIMT@users.noreply.github.com>
2023-12-11 09:19:37 +0100

    Add files via upload

---
+++ projet_meteo/src/main/java/com/example/projet_meteo/ProjetMeteoApplication.java
@@ -0,0 +1,13 @@
+package com.example.projet_meteo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetMeteoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetMeteoApplication.class, args);
+	}
+
+}
