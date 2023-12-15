fb157efbd153fcb72b169da7a3176f680001e05c
Malo BEUCHEE <malo.beuchee@etu.imt-nord-europe.fr>
2023-12-15 10:26:41 +0100

    ancienne popup vers popup alert message. quelque bug encore

--- projet_meteo/src/main/js/popup.js
+++ projet_meteo/src/main/js/popup.js
@@ -6,3 +6,12 @@ text = "User cancelled the prompt.";
 } else {
 text = "Hello " + person + "! How are you today?";
 }
+}
+
+        function openForm2(){
+             let element = document.getElementById("login");
+             if (element.style.display =="block") {
+                   document.getElementById("login").style.display = "none";
+             }else ( element.style.display ="block");
+            console.log("open");
+        }
