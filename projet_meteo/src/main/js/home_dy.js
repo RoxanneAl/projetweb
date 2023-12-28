7a2c9ad1ff621171c88f17d2bd012c09475881e7
Malo BEUCHEE <malo.beuchee@etu.imt-nord-europe.fr>
2023-12-28 12:14:10 +0100

    changer la couleur de base des élément navbar et button.

--- projet_meteo/src/main/js/home_dy.js
+++ projet_meteo/src/main/js/home_dy.js
@@ -1,9 +1,7 @@
 function openForm() {
     document.getElementById("popupForm").style.display = "block";
 }
-function loginForm(){
-let
-}
+
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
