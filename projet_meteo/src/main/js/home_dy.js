a61ac8ae5e19fe450fed4134e78e63cc184f475c
redmind-doc <malo.beuchee@gmail.com>
2024-01-15 14:03:11 +0100

    ajout map center sur la france.

--- projet_meteo/src/main/js/home_dy.js
+++ projet_meteo/src/main/js/home_dy.js
@@ -5,3 +5,17 @@ function openForm() {
 function closeForm() {
     document.getElementById("popupForm").style.display = "none";
 }
+
+let map;
+// initMap is now async
+async function initMap() {
+    // Request libraries when needed, not in the script tag.
+    const { Map } = await google.maps.importLibrary("maps");
+    // Short namespaces can be used.
+    map = new Map(document.getElementById("map"), {
+        center: { lat: 46.86, lng: 3 },
+        zoom: 6.66,
+    });
+}
+
+initMap()
